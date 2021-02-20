502ada40d19cdd83dffab950546ab3c27f4a454f
Destiny Hochhalter <destinyhoch@gmail.com>
2021-02-19 20:39:09 -0800

    Deleted story board

--- Zoom Scheduler/SceneDelegate.swift
+++ Zoom Scheduler/SceneDelegate.swift
@@ -17,6 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+    
+                if let windowScene = scene as? UIWindowScene {
+                    window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+                    window?.windowScene = windowScene
+                    window?.rootViewController = MainVC()
+                    window?.makeKeyAndVisible()
+                }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
