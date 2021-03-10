72f02ea6f3d2220ade95942e59a37c9831ad3e74
Destiny Hochhalter-Ruiz <destinyhoch@gmail.com>
2021-03-10 01:34:32 -0800

    Added unit tests for scheduling notification

--- Zoom SchedulerTests/Zoom_SchedulerTests.swift
+++ Zoom SchedulerTests/Zoom_SchedulerTests.swift
@@ -97,31 +97,135 @@ class Zoom_SchedulerTests: XCTestCase {
         XCTAssertEqual(isValid, true)
     }
     
+    // test fcuntion that creates time components for a notification
+    func testGetComponents1() {
+        let meeting = ZoomMeeting(meetingName: "test", professorName: "test", startTime: 0, endTime: 1, meetingURL: "https://google.com", dayOfWeek: 0)
+        
+        let components = getComponents(meeting: meeting)
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        XCTAssertEqual(day, 2) // Monday
+        XCTAssertEqual(hour, 1)
+        XCTAssertEqual(minute, 0)
+    }
+    
+    func testGetComponents2() {
+        let meeting = ZoomMeeting(meetingName: "test", professorName: "test", startTime: 40, endTime: 46, meetingURL: "https://google.com", dayOfWeek: 1)
+        
+        let components = getComponents(meeting: meeting)
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        XCTAssertEqual(day, 3) // Tuesday
+        XCTAssertEqual(hour, 4)
+        XCTAssertEqual(minute, 20)
+    }
+    
+    func testGetComponents3() {
+        let meeting = ZoomMeeting(meetingName: "test", professorName: "test", startTime: 200, endTime: 205, meetingURL: "https://google.com", dayOfWeek: 5)
+        
+        let components = getComponents(meeting: meeting)
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        XCTAssertEqual(day, 0) // Saturday
+        XCTAssertEqual(hour, 17)
+        XCTAssertEqual(minute, 40)
+    }
+    func testGetComponents4() {
+        let meeting = ZoomMeeting(meetingName: "test", professorName: "test", startTime: 13, endTime: 16, meetingURL: "https://google.com", dayOfWeek: 3)
+        
+        let components = getComponents(meeting: meeting)
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        XCTAssertEqual(day, 5) // Thursday
+        XCTAssertEqual(hour, 2)
+        XCTAssertEqual(minute, 5)
+    }
+    
+    func testGetComponents5() {
+        let meeting = ZoomMeeting(meetingName: "test", professorName: "test", startTime: 256, endTime: 259, meetingURL: "https://google.com", dayOfWeek: 4)
+        
+        let components = getComponents(meeting: meeting)
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        XCTAssertEqual(day, 6) // Saturday
+        XCTAssertEqual(hour, 22)
+        XCTAssertEqual(minute, 20)
+    }
+    
+    func testGetComponents6() {
+        // invalid start and end times
+        let meeting = ZoomMeeting(meetingName: "test", professorName: "test", startTime: 200, endTime: 205, meetingURL: "https://google.com", dayOfWeek: 8)
+        
+        let components = getComponents(meeting: meeting)
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        XCTAssertEqual(day, nil)
+        XCTAssertEqual(hour, nil)
+        XCTAssertEqual(minute, nil)
+    }
+    
+    func testGetComponents7() {
+        // invalid start and end times, and invalid week day
+        let meeting = ZoomMeeting(meetingName: "test", professorName: "test", startTime: 400, endTime: 404, meetingURL: "https://google.com", dayOfWeek: 9)
+        
+        let components = getComponents(meeting: meeting)
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        XCTAssertEqual(day, nil)
+        XCTAssertEqual(hour, nil)
+        XCTAssertEqual(minute, nil)
+    }
+    
+    func testGetComponents8() {
+        // invalid start and end times with negative numbers
+        let meeting = ZoomMeeting(meetingName: "test", professorName: "test", startTime: -16, endTime: -20, meetingURL: "https://google.com", dayOfWeek: 3)
+        
+        let components = getComponents(meeting: meeting)
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        XCTAssertEqual(day, nil)
+        XCTAssertEqual(hour, nil)
+        XCTAssertEqual(minute, nil)
+    }
+    
+    func testGetComponents9() {
+        // invalid day with negative int
+        let meeting = ZoomMeeting(meetingName: "test", professorName: "test", startTime: 16, endTime: 20, meetingURL: "https://google.com", dayOfWeek: -3)
+        
+        let components = getComponents(meeting: meeting)
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        XCTAssertEqual(day, nil)
+        XCTAssertEqual(hour, nil)
+        XCTAssertEqual(minute, nil)
+    }
+    
+    func testGetComponents10() {
+        // test with both negative intergers for start/end time and day
+        let meeting = ZoomMeeting(meetingName: "test", professorName: "test", startTime: -16, endTime: -20, meetingURL: "https://google.com", dayOfWeek: -3)
+        
+        let components = getComponents(meeting: meeting)
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        XCTAssertEqual(day, nil)
+        XCTAssertEqual(hour, nil)
+        XCTAssertEqual(minute, nil)
+    }
+    
+    
+    
 }
     
-//    func testDayDictValid() {
-//        let dictValue = weekDays[0]
-//        XCTAssertEqual(dictValue, "Monday")
-//    }
-//
-//    func testDayDictInvalid() {
-//        let dictValue = weekDays[10]
-//        XCTAssertEqual(dictValue, nil)
-//    }
-//
-//    func testTimeDictValid() {
-//        let dictValue = timeOfDay[131]
-//        XCTAssertEqual(dictValue, "11:55")
-//    }
-//
-//    func testTimeDictInvalid() {
-//        let dictValue = timeOfDay[1000]
-//        XCTAssertEqual(dictValue, nil)
-//    }
-//
-
-//}
-
     /*
       func testValidURL6() {
         let url = createURL(url: "https://www.234234242.com") //Testing Invalid URL
